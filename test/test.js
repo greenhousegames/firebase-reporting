890fee8eb74e05eee9368df5ee79d984809c7eb2
Brian Soumakian <bsoumakian@gmail.com>
2016-10-05 15:03:06 -0700

    get tests to pass

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
-var expect = require('chai').expect;
-var FirebaseReporting = require('../dist/index.js');
-var proxyquire = require('proxyquire');
+var PORT = 45000;
 var originalWebsocket = require('faye-websocket');
+var expect = require('chai').expect;
+var proxyquire = require('proxyquire');
 
 // Firebase has strict requirements about the hostname format. So we provide a dummy
 // hostname and then change the URL to localhost inside the faye-websocket's Client
 // constructor.
 var firebase = proxyquire('firebase', {
   'faye-websocket': {
-    Client: function (url) {
+    Client: (url) => {
       url = url.replace(/dummy\d+\.firebaseio\.test/i, 'localhost');
       return new originalWebsocket.Client(url);
     },
@@ -18,44 +18,60 @@ var firebase = proxyquire('firebase', {
 var FirebaseServer = require('firebase-server');
 
 describe('Firebase Reporting', () => {
-  var PORT = 45000;
   var firebaseServer;
 	var sequentialConnectionId = 0;
   var helpers = {
     newServerUrl: () => {
   		return 'ws://dummy' + (sequentialConnectionId++) + '.firebaseio.test:' + PORT;
   	},
-    createClient: () => {
-      return firebase.initializeApp({
-        databaseURL: helpers.newServerUrl(),
-        serviceAccount: {
-          'private_key': 'fake',
-          'client_email': 'fake'
-        }
-      }, 'test-firebase-client-' + sequentialConnectionId);
-    },
     createServer: () => {
       return new FirebaseServer(PORT, 'localhost:' + PORT);
     }
   };
 
   beforeEach(() => {
-    firebaseServer = helpers.createServer();
   });
 
-  afterEach(function () {
+  afterEach(() => {
     if (firebaseServer) {
       firebaseServer.close();
       firebaseServer = null;
     }
   });
 
-  it('firebase should be configured for testing', function (done) {
-    const client = helpers.createClient();
-    const ref = client.database().ref();
-		ref.once('value', function (snap) {
-      expect(snap.val()).to.be.null;
-			done();
-		});
-	});
+  function newFirebaseClient() {
+		var name = 'test-firebase-client-' + sequentialConnectionId;
+		var url = 'ws://dummy' + (sequentialConnectionId++) + '.firebaseio.test:' + PORT;
+		var config = {
+			databaseURL: url
+		};
+		var app = firebase.initializeApp(config, name);
+		return app.database().ref();
+	}
+
+  function newFirebaseServer(data) {
+    return new FirebaseServer(PORT, 'localhost:' + PORT, data);
+  }
+
+  describe('Testing Setup', () => {
+    it('client should connect to server', (done) => {
+      firebaseServer = newFirebaseServer();
+      const client = newFirebaseClient();
+  		client.once('value').then((snap) => {
+        expect(snap.val()).to.be.null;
+  			done();
+  		});
+  	});
+
+    it('client should read data from server', (done) => {
+      firebaseServer = newFirebaseServer();
+      const client = newFirebaseClient();
+      client.set('hello').then(() => {
+        client.once('value').then((snap) => {
+          expect(snap.val()).to.equal('hello');
+    			done();
+    		});
+      });
+  	});
+  });
 });
